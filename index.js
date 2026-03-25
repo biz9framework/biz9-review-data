05ff5b3dba25fe1e129922a7c54447984a9c5f0c
think1 <think1@localhost.localdomain>
2026-03-25 13:04:19 -0400

    good

--- index.js
+++ index.js
@@ -12,139 +12,124 @@ const {Review_Field,Review_Table,Review_Logic}=require("/home/think1/www/doqbox/
 const {Data_Logic,Data_Field,Data_Value_Type} = require("/home/think1/www/doqbox/biz9-framework/biz9-data-logic/source");
 const {Log,Str,Obj,DateTime}=require("/home/think1/www/doqbox/biz9-framework/biz9-utility/source");
 class Review_Data {
-	//9_review_post /9_post
-	static post = async(database,parent_table,parent_id,user_id,post_review,option) => {
-		return new Promise((callback) => {
-			let response = {};
-			let data = {};
-			let review = Review_Logic.get(parent_table,parent_id,user_id,post_review.title,post_review.comment,post_review.rating);
-			option = !Obj.check_is_empty(option) ? option : {};
-			async.series([
-				//review_post
-				async function(call){
-					const [biz_response,biz_data] = await Data.post(database,Review_Table.REVIEW,review);
-					data.review = biz_data;
-				},
-				async function(call){
-					const [biz_response,biz_data] = await Review_Data.caculate(database,parent_table,parent_id);
-					data.parent = biz_data;
-				},
-				//post_item
-                /*
-				async function(call){
-					if(!Str.check_is_null(data.parent.id)){
-						//rating_count
-						data.parent.rating_count = !Str.check_is_null(data.parent.rating_count) ? parseInt(data.parent.rating_count) + parseInt(review.rating) :parseInt(review.rating);
-						//review_count
-						data.parent.review_count = !Str.check_is_null(data.parent.review_count) ? parseInt(data.parent.review_count) + 1 : 1;
-						//rating_avg
-						data.parent.rating_avg = !Str.check_is_null(data.parent.rating_avg) ? parseInt(data.parent.rating_count)  /  parseInt(data.parent.review_count) :parseInt(review.rating);
-						const [biz_response,biz_data] = await Data.post(database,parent_table,data.parent);
-						data.parent = biz_data;
-					}
-				},
-                */
-		]).then(result => {
-				callback([response,data]);
-			}).catch(err => {
-				Log.error("Review-Data",err);
-			});
-		});
-	};
-	//9_review_search 9_search
-	static search = (database,filter,sort_by,page_current,page_size,option) => {
-		return new Promise((callback) => {
-			let data = {item_count:0,page_count:1,filter:{},table:Review_Table.REVIEW,reviews:[]};
-			let response = {};
-			option = !Obj.check_is_empty(option)  ? option : {};
-			async.series([
-				async function(call){
-					const [biz_response,biz_data] = await Data.search(database,Review_Table.REVIEW,filter,sort_by,page_current,page_size,option);
-					data.item_count = biz_data.item_count;
-					data.page_count = biz_data.page_count;
-					data.search = biz_data.search;
-					data.reviews = biz_data[Type.FIELD_ITEMS];
-				},
-			]).then(result => {
-				callback([response,data]);
-			}).catch(err => {
-				Log.error("Review-Search",err);
-			});
-		});
-	};
-	//9_review_get 9_get
-	static get = async (database,parent_table,parent_id,sort_by,page_current,page_size) => {
-		return new Promise((callback) => {
-			let response = {};
-			let data = {};
-			async.series([
-				//reviews
-				async function(call){
-					let query = {parent_id:parent_id,parent_table:parent_table};
-					let search = Data_Logic.get_search(Review_Table.REVIEW,query,sort_by,page_current,page_size);
-					let foreign_user = Data_Logic.get_foreign(Data_Value_Type.ONE,Review_Table.USER,Data_Field.ID,Data_Field.USER_ID,{title:'user'});
-					let option = {foreigns:[foreign_user]};
-					const [biz_response,biz_data] = await Data.search(database,search.table,search.filter,search.sort_by,search.page_current,search.page_size,option);
-					data.item_count=biz_data.item_count;
-					data.page_count=biz_data.page_count;
-					data.search=biz_data.search;
-					data.reviews=biz_data[Data_Field.ITEMS];
-				},
-			]).then(result => {
-				callback([response,data]);
-			}).catch(err => {
-				Log.error("Review-Data-List",err);
-				callback([err,[]]);
-			});
-		});
-	};
-	//9_review_delete /9_delete
-	static delete = async(database,parent_table,parent_id,review_id) => {
-		return new Promise((callback) => {
-			let response = {};
-			let data = {parent:Data_Logic.get(parent_table,parent_id),review:Data_Logic.get(Review_Table.REVIEW,0)};
-			let review = Data_Logic.get(Review_Table.REVIEW,review_id);
-			async.series([
-				//review_post
-				async function(call){
-					const [biz_response,biz_data] = await Data.delete(database,Review_Table.REVIEW,review.id);
+    //9_review_post /9_post
+    static post = async(database,parent_table,parent_id,user_id,post_review,option) => {
+        return new Promise((callback) => {
+            let response = {};
+            let data = {};
+            let review = Review_Logic.get(parent_table,parent_id,user_id,post_review.title,post_review.comment,post_review.rating);
+            option = !Obj.check_is_empty(option) ? option : {};
+            async.series([
+                async function(call){
+                    //review
+                    const [biz_response,biz_data] = await Data.post(database,Review_Table.REVIEW,review);
+                    data.review = biz_data;
+                },
+                async function(call){
+                    //parent
+                    const [biz_response,biz_data] = await Review_Data.caculate(database,parent_table,parent_id);
+                    data.parent = biz_data;
+                },
+            ]).then(result => {
+                callback([response,data]);
+            }).catch(err => {
+                Log.error("Review-Data",err);
+            });
+        });
+    };
+    //9_review_search 9_search
+    static search = (database,filter,sort_by,page_current,page_size,option) => {
+        return new Promise((callback) => {
+            let data = {item_count:0,page_count:1,filter:{},table:Review_Table.REVIEW,reviews:[]};
+            let response = {};
+            option = !Obj.check_is_empty(option)  ? option : {};
+            async.series([
+                async function(call){
+                    const [biz_response,biz_data] = await Data.search(database,Review_Table.REVIEW,filter,sort_by,page_current,page_size,option);
+                    data.item_count = biz_data.item_count;
+                    data.page_count = biz_data.page_count;
+                    data.search = biz_data.search;
+                    data.reviews = biz_data[Type.FIELD_ITEMS];
+                },
+            ]).then(result => {
+                callback([response,data]);
+            }).catch(err => {
+                Log.error("Review-Search",err);
+            });
+        });
+    };
+    //9_review_get 9_get
+    static get = async (database,parent_table,parent_id,sort_by,page_current,page_size) => {
+        return new Promise((callback) => {
+            let response = {};
+            let data = {};
+            async.series([
+                //reviews
+                async function(call){
+                    let query = {parent_id:parent_id,parent_table:parent_table};
+                    let search = Data_Logic.get_search(Review_Table.REVIEW,query,sort_by,page_current,page_size);
+                    let foreign_user = Data_Logic.get_foreign(Data_Value_Type.ONE,Review_Table.USER,Data_Field.ID,Data_Field.USER_ID,{title:'user'});
+                    let option = {foreigns:[foreign_user]};
+                    const [biz_response,biz_data] = await Data.search(database,search.table,search.filter,search.sort_by,search.page_current,search.page_size,option);
+                    data.item_count=biz_data.item_count;
+                    data.page_count=biz_data.page_count;
+                    data.search=biz_data.search;
+                    data.reviews=biz_data[Data_Field.ITEMS];
+                },
+            ]).then(result => {
+                callback([response,data]);
+            }).catch(err => {
+                Log.error("Review-Data-List",err);
+                callback([err,[]]);
+            });
+        });
+    };
+    //9_review_delete /9_delete
+    static delete = async(database,parent_table,parent_id,review_id) => {
+        return new Promise((callback) => {
+            let response = {};
+            let data = {parent:Data_Logic.get(parent_table,parent_id),review:Data_Logic.get(Review_Table.REVIEW,0)};
+            let review = Data_Logic.get(Review_Table.REVIEW,review_id);
+            async.series([
+                //review_post
+                async function(call){
+                    const [biz_response,biz_data] = await Data.delete(database,Review_Table.REVIEW,review.id);
                     Log.w('22_review_delete',biz_data);
                     Log.w('22_review_delete',biz_response);
-					data.review = biz_data;
-				},
-            	async function(call){
-					const [biz_response,biz_data] = await Review_Data.caculate(database,parent_table,parent_id);
-					data.parent = biz_data;
-				},
-			]).then(result => {
-				callback([response,data]);
-			}).catch(err => {
-				Log.error("Review-Data-Delete",err);
-			});
-		});
-	};
+                    data.review = biz_data;
+                },
+                async function(call){
+                    const [biz_response,biz_data] = await Review_Data.caculate(database,parent_table,parent_id);
+                    data.parent = biz_data;
+                },
+            ]).then(result => {
+                callback([response,data]);
+            }).catch(err => {
+                Log.error("Review-Data-Delete",err);
+            });
+        });
+    };
     //9_parent_review_caculate 9_parent_caculate
-	static caculate = (database,parent_table,parent_id) => {
-		return new Promise((callback) => {
-			let data = {};
-			let response = {};
+    static caculate = (database,parent_table,parent_id) => {
+        return new Promise((callback) => {
+            let data = {};
+            let response = {};
             let parent = {};
             let reviews = [];
-			async.series([
-			    //get_parent
-				async function(call){
-                    Log.w('11_parent_table',parent_table);
-                    Log.w('11_parent_id',parent_id);
-					const [biz_response,biz_data] = await Data.get(database,parent_table,parent_id);
-					parent = biz_data;
-				},
-				async function(call){
+            async.series([
+                async function(call){
+                    //parent
+                    const [biz_response,biz_data] = await Data.get(database,parent_table,parent_id);
+                    parent = biz_data;
+                },
+                async function(call){
+                    //reviews
                     let search = Data_Logic.get_search(Review_Table.REVIEW,{parent_id:parent_id},{},1,0);
-					const [biz_response,biz_data] = await Data.search(database,search.table,search.filter,{},search.page_current,search.page_size);
-					reviews = biz_data.items;
-                    Log.w('review_count',reviews.length);
-				},
-	            async function(call){
+                    const [biz_response,biz_data] = await Data.search(database,search.table,search.filter,{},search.page_current,search.page_size);
+                    reviews = biz_data.items;
+                },
+                async function(call){
+                    //caculate
                     let rating_count = 0;
                     let review_count = reviews.length;
                     let rating_avg = 0;
@@ -154,27 +139,22 @@ class Review_Data {
                                 review.rating = 0;
                             }
                             rating_count = parseInt(rating_count) + parseInt(review.rating);
-                            //rating_count = !Str.check_is_null(parent.rating_count) ? parseInt(parent.rating_count) + parseInt(review.rating) : parseInt(review.rating);
-	                        //rating_avg = !Str.check_is_null(parent.rating_avg) ? parseInt(parent.rating_count)  /  parseInt(parent.review_count) :parseInt(review.rating);
                         }
                     }
-                    parent.rating_count = rating_count; // = items count
-                    parent.review_count = review_count; //
+                    parent.rating_count = rating_count;
+                    parent.review_count = review_count;
                     parent.rating_avg = parent.rating_count / parent.review_count;
-                    Log.w('33_caculate_parent',parent);
-					const [biz_response,biz_data] = await Data.post(database,parent.table,parent);
-					data.parent = biz_data;
+                    const [biz_response,biz_data] = await Data.post(database,parent.table,parent);
+                    data.parent = biz_data;
                 },
-			]).then(result => {
-				callback([response,parent]);
-			}).catch(err => {
-				Log.error("Review-Caculate",err);
-			});
-		});
-	};
-
+            ]).then(result => {
+                callback([response,parent]);
+            }).catch(err => {
+                Log.error("Review-Caculate",err);
+            });
+        });
+    };
 }
-
 module.exports = {
     Review_Data
 };
